a45ac592d35ca773842b847e1bcd8ffc6129612d
Ashish Choudhary <ashishdangi96@gmail.com>
2022-09-30 17:22:02 +0530

    kk

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,4 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
 
   $receiving_email_address = 'codebyashish@gmail.com';
