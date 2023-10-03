85bf2c73dc73bd26d56cb2883df2d693beaa5f81
Ashish <ashishdangi96@gmail.com>
2023-10-03 17:01:07 +0530

    server added

---
+++ assets/netlify/functions/updateData.js
@@ -0,0 +1,26 @@
+export async function handler(event) {
+    try {
+      const fs = require('fs');
+      const data = JSON.parse(fs.readFileSync('/assets/appdata/urls.json', 'utf8'));
+  
+      // Parse the incoming request body (assumes JSON)
+      const requestBody = JSON.parse(event.body);
+  
+      // Update the JSON data
+      data.someKey = requestBody.newValue;
+  
+      // Write the updated data back to the file
+      fs.writeFileSync('/assets/appdata/urls.json', JSON.stringify(data));
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'JSON data updated successfully' }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'Error updating JSON data' }),
+      };
+    }
+  }
+  
