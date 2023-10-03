700233e8ac3527e26c04fb9b058a1bc4f8f9c9a5
Ashish <ashishdangi96@gmail.com>
2023-10-03 17:19:05 +0530

    ss

--- netlify/functions/updateData.js
+++ netlify/functions/updateData.js
@@ -17,6 +17,7 @@ export async function handler(event) {
         body: JSON.stringify({ message: 'JSON data updated successfully' }),
       };
     } catch (error) {
+      console.log("ashishji " + error)
       return {
         statusCode: 500,
         body: JSON.stringify({ message: 'Error updating JSON data' }),
