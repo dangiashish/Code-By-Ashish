e213093fcbedad67768e56fc4fa7927c8742b5aa
Ashish <ashishdangi96@gmail.com>
2023-10-03 18:27:17 +0530

    dd

--- netlify/functions/updateData.js
+++ netlify/functions/updateData.js
@@ -26,37 +26,3 @@
 //   }
   
 
-const fs = require('fs');
-const path = require('path');
-
-exports.handler = async (event) => {
-  try {
-    // Use path.join to create a correct relative path
-    const filePath = '/appurl.json';
-
-    
-    // Parse the incoming request body (assumes JSON)
-    const requestBody = JSON.parse(event.body);
-
-    // Read the existing JSON file
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
-    // Append the new data to the JSON array
-    data.push(requestBody);
-
-    // Write the updated data back to the file
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'JSON data updated successfully' }),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error updating JSON data' , error : error}),
-    };
-  }
-};
-
